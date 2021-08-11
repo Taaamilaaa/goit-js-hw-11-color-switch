c98085c8056b11163494e1f4ec385a8eb6d278f7
Yudina Tamila <taaamilaaa1988@gmail.com>
2021-08-11 18:36:01 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const refs = {
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+    body: document.querySelector('body')
+};
+
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+refs.body.classList.
+
+refs.startBtn.addEventListener('click', switcher);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function switcher(arr) {
+   
+refs.body.classList.add()
+}
+
+
+
+
+
+
+
+
+
+
