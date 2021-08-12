e542a792445cfeea927d14f3d567a9052cf346bd
Yudina Tamila <taaamilaaa1988@gmail.com>
2021-08-12 08:32:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 const refs = {
-    startBtn: document.querySelector('[data-action="start"]'),
-    stopBtn: document.querySelector('[data-action="stop"]'),
-    body: document.querySelector('body'),
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
 };
 
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
-
-// console.log(colors[5]);
 console.log(refs.body);
-console.log(colors[5]);
-//генерация случайного цвета из массива
-//навесить на кнопку старта  слушателя
-//описать функцию сет интервал
-
-
-
-
-
-
+console.log(refs.startBtn);
+console.log(refs.stopBtn);
+console.log(refs.body);
